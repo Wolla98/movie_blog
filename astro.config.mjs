66cf985a304ceaad4401a127103c49d840a63fb9
Wolla98 <winstonbounsavy@gmail.com>
2025-07-09 01:30:46 -0500

    Fixed base hopefully

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://Wolla98.github.io/",
-	base: "",
+	base: "/movie_blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
